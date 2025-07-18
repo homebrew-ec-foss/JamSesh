01b51566c481c82eb7e1d6781f42554710064072
Vinaayak G Dasika <64347927+Delta18-Git@users.noreply.github.com>
2025-07-18 12:47:49 +0530

    docs: add pull request template (#10)
    adds a PR template for more structure
    
    feat: add signaling and vid call
    
    chore: add server.js
    
    chore: add package.json and server.js, remove obsolete project files
    
    chore: add ws dependency and .gitignore to ignore node_modules
    
    feat: Initialize Webscoket connection
    
    feat: Add caller and callee function
    
    fix: Made .gitignore global
    
    fix: Make message to string in server.js
    
    fix: extra line
    
    test: add basic client.js to test, add test video rec
    
    feat: update index.html and PeerConnection.js
    
    refactor: changed websocket server port to use dynamic render port
    
    refactor: changed websocket client instance to use render-hosted server
    
    refactor: changed PeerConnection.js to use render-hosted server
    
    refactor: Add render server
    
    feat: add html page
    
    refactor: Change console message
    
    feat: Add frontend, implement screensharing with audio sharing only

---
+++ signaling-server/server.js
@@ -0,0 +1,32 @@
+// Loading ws (websocket) module
+const WebSocket = require('ws');
+
+// Defining port as dynamic render port
+const PORT = process.env.PORT || 8080;
+
+// Creating new websocket server instance
+const wss = new WebSocket.Server({port: PORT});
+
+// Handling incoming client connections
+wss.on('connection', function connection(ws) {    // Registering event handler (fn runs when client connects)
+    ws.on('message', function incoming(message) {    // Setting event listener for message events (whenever client sends data)
+        const messageString = message.toString();
+        console.log(`Received and broadcasting: ${messageString}`); 
+        // Broadcasting data to all connected clients
+        wss.clients.forEach(function each(client) {    // Looping over all connected clients
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(messageString);    // Sending data to all connected clients
+            }
+        });
+    });
+        ws.on('close', () => {
+        console.log("[Server] Client disconnected.");
+    });
+
+    ws.on('error', (error) => {
+        console.error("[Server] WebSocket error:", error);
+    });
+});
+
+// Printing confirmation message to terminal when server goes live
+console.log("WebSocket server running on wss://jamsesh-8wui.onrender.com");
