708b6a60bdfe4bc01c15e2378b91a96c5ca966d6
Vanshitha <vanshitha.s@gmail.com>
2025-07-22 19:57:43 +0530

    feat: Add caller and callee function

--- public/js/PeerConnction.js
+++ public/js/PeerConnction.js
@@ -2,7 +2,7 @@ let ws;
 let peerConnection;
 const offerBtn = document.getElementById('offerBtn');
 const answerBtn = document.getElementById('answerBtn');
-const receivedSdp = document.getElementById('receivedSdp');
+const rcvSdp = document.getElementById('rcvSdp');
 
 const iceServers = [
     { urls: "stun:stun.l.google.com:19302" },
@@ -31,17 +31,21 @@ const init = () => {
         console.log(`Received WS message: Type=${data.type || 'N/A'}`, data);
         if (data.type === 'offer') {
             // If offer is recieved, we are the callee
-            receivedSdp.textContent = JSON.stringify(data.sdp, null, 2);
+            rcvSdp.textContent = JSON.stringify(data.sdp, null, 2);
             answerBtn.disabled = false;
             console.log('Offer received');
         }
         else if (data.type === 'answer') {
             // If answer is recieved, we are the caller
-            receivedSdp.textContent = JSON.stringify(data.sdp, null, 2);
+            rcvSdp.textContent = JSON.stringify(data.sdp, null, 2);
             console.log('Answer received. SDP exchange complete');
         }
+        else if (data.type === 'candidate') {
+            // for testing
+            console.log('ICE candidated received');
+        }
         else {
-            // Could be either Ice candidate or any other unrecognized message
+            // unrecognized message
             console.log('Not recognized');
         }
     };
@@ -56,3 +60,80 @@ const init = () => {
         console.error('WebSocket error:', err);
     };
 };
+
+
+// WebRTC SDP functions
+
+// 1. caller creates offer and sends it over
+const sendOffer = async () => {
+    
+    //initialized the peer connection
+    peerConnection = new RTCPeerConnection(iceServers);
+    console.log('PeerConnection initialized for offer');
+    // simulating sending of media stream
+    peerConnection.addTransceiver('audio', { direction: 'recvonly' });
+    peerConnection.addTransceiver('video', { direction: 'recvonly' });
+
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            // only testing for ice candidates
+            console.log('Ice candidates', event.candidate.candidate);
+            ws.send(JSON.stringify({ type: 'candidate', candidate: event.candidate }));
+        }
+    };
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+
+    console.log('Created and sent Offer:', peerConnection.localDescription);
+    ws.send(JSON.stringify({ type: 'offer', sdp: peerConnection.localDescription }));
+    offerBtn.disabled = true;
+};
+
+// 2. callee processes offer and sends answer
+const rcvOffer = async () => {
+
+    if (!peerConnection) {
+        // initialized the peer connection
+        peerConnection = new RTCPeerConnection(iceServers);
+        console.log('PeerConnection initialized for answer.');
+        // simulating sending of media stream
+        peerConnection.addTransceiver('audio', { direction: 'recvonly' });
+        peerConnection.addTransceiver('video', { direction: 'recvonly' });
+
+        peerConnection.onicecandidate = (event) => {
+            // only testing for ice candidates
+            if (event.candidate) {
+                console.log('Ice candidates', event.candidate.candidate);
+                ws.send(JSON.stringify({ type: 'candidate', candidate: event.candidate }));
+            }
+        };
+    }
+
+    // recieved the SDP
+    const rcvSdpText = rcvSdp.textContent;
+    if (!rcvSdpText) {
+        console.error('No offer received');
+        return;
+    }
+    const rcvOfferSdp = JSON.parse(rcvSdpText);
+
+    // set remote Offer
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(rcvOfferSdp));
+    console.log('Remote offer');
+
+    // create and set local Answer
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    console.log('created and set local answer:', peerConnection.localDescription);
+    ws.send(JSON.stringify({ type: 'answer', sdp: peerConnection.localDescription }));
+    statusSpan.textContent = 'Answer sent, SDP exchange complete.';
+    answerBtn.disabled = true;
+};
+
+// button function
+offerBtn.addEventListener('click', sendOffer);
+answerBtn.addEventListener('click', rcvOffer);
+window.onload = init;
