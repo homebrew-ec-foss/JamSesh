357d477f33b0ef32ee297d158a381071540d381a
Vanshitha <vanshitha.s@gmail.com>
2025-07-22 20:29:25 +0530

    fix: Make message to string in server.js

--- signaling-server/server.js
+++ signaling-server/server.js
@@ -7,12 +7,21 @@ const wss = new WebSocket.Server({port: 8080});
 // Handling incoming client connections
 wss.on('connection', function connection(ws) {    // Registering event handler (fn runs when client connects)
     ws.on('message', function incoming(message) {    // Setting event listener for message events (whenever client sends data)
+        const messageString = message.toString();
+        console.log(`Received and broadcasting: ${messageString}`); 
         // Broadcasting data to all connected clients
         wss.clients.forEach(function each(client) {    // Looping over all connected clients
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);    // Sending data to all connected clients
+                client.send(messageString);    // Sending data to all connected clients
             }
         });
+    });
+        ws.on('close', () => {
+        console.log("[Server] Client disconnected.");
+    });
+
+    ws.on('error', (error) => {
+        console.error("[Server] WebSocket error:", error);
     });
 });
 
