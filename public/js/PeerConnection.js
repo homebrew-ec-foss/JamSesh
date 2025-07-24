1b92a92d659102b7e9ab695cf00de2f856afe509
Vanshitha <vanshitha.s@gmail.com>
2025-07-24 13:27:19 +0530

    refactor: changed PeerConnection.js to use render-hosted server

--- public/js/PeerConnection.js
+++ public/js/PeerConnection.js
@@ -20,7 +20,7 @@ const iceServers = [
 //initialization and creation of websocket connection
 
 const init = () => {
-    ws = new WebSocket("ws://localhost:8080");
+    ws = new WebSocket("wss://jamsesh.onrender.com");
     ws.onopen = () => {
         console.log("Websocket connected");
         offerBtn.disabled = false;
