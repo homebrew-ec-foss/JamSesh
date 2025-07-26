d08352d0c848d754aa7a1a44f65cb5cddc7dd5c8
Gowda-Arjun <209913189+Gowda-Arjun@users.noreply.github.com>
2025-07-26 19:15:45 +0530

    feat: added a turn server onto iceServers list

--- system-aud+vid_stream/PeerConnection.js
+++ system-aud+vid_stream/PeerConnection.js
@@ -13,7 +13,12 @@ const iceServers = [
     { urls: "stun:stun3.l.google.com:3478" },
     { urls: "stun:stun3.l.google.com:5349" },
     { urls: "stun:stun4.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:5349" }
+    { urls: "stun:stun4.l.google.com:5349" },
+    {
+    url: 'turn:turn.bistri.com:80',
+    credential: 'homeo',
+    username: 'homeo'
+    }
 ];
 
 //initialization and creation of websocket connection
